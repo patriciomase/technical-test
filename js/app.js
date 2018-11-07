04548a4b0a8b2f44a52fc294d2a5f0d440baff16
Patricio G Maseda <pmaseda@sparkdigital.com>
2018-11-06 21:48:46 -0300

    Make class names more declarative

--- js/app.js
+++ js/app.js
@@ -5,19 +5,19 @@ function loadTasks(taskList){
 
     taskList.forEach((task,i)=>{
         let divTask = document.createElement('div');
-        divTask.className="taskCard";
+        divTask.className="task";
         divTask.innerHTML = `
             ${task.img?"<img src='" + task.img + "' />":""}
             <h4> <input type="checkbox" ${task.completed?"checked":""} title="check completed">  ${task.title + ' ' + i}</h4>
             <span>created on ${task.createdOn} by ${task.createdBy}</span>
             <p>${task.description}</p>
-            <span>Due on ${task.dueDate}</span>            
+            <span>Due on ${task.dueDate}</span>
         `;
 
         if(task.completed)
-            divTask.classList.add("taskCompleted");
+            divTask.classList.add("completed");
         else if(task.dueDate < Date.now() )
-            divTask.classList.add("taskLate");
+            divTask.classList.add("late");
 
 
         container.appendChild(divTask);
